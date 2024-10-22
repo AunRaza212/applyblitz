41799f51015f1b2a542e1b56417383656caa8def
Aun Raza <aunr76@gmail.com>
2024-10-22 16:22:47 +0500

    Whatsapp button

--- components/WhatsappButton.js
+++ components/WhatsappButton.js
@@ -4,15 +4,15 @@ import Image from 'next/image';
 import image from '../public/whatsapp.svg'
 import '../styles/global.css'
 export default function WhatsAppChat() {
-  const whatsappLink = "https://wa.me/+923342785830";
+    const whatsappLink = "https://wa.me/+923342785830";
 
-  return (
-    <div>
-      {/* WhatsApp Floating Button */}
-      <a aria-label="Chat on WhatsApp" href={whatsappLink} target="_blank" rel="noopener noreferrer" className="whatsapp_float">
-     
-        <span>Chat on WhatsApp</span>
-      </a>
-    </div>
-  );
+    return (
+        <div>
+            {/* WhatsApp Floating Button */}
+            <a aria-label="Chat on WhatsApp" href={whatsappLink} target="_blank" rel="noopener noreferrer" className="whatsapp_float">
+
+                <span>Chat on WhatsApp</span>
+            </a>
+        </div>
+    );
 }
